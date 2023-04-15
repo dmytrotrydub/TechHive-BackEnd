69257f72ceb02681604125e10c5b5f2b52050a45
dmytrotrydub <tridub.d87@gmail.com>
2023-04-14 19:38:16 -0700

    cleaning some files

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
 app.get('/', function (req, res) {
   res.send('Hello').status(200);
 });
-
+console.log(process.env.PORT);
 app.listen(8080, function (req, res) {
   console.log(`App listening at http: ${process.env.PORT}`);
 });
